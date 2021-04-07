26712f5527a52a47d76aa58081eea469633fd395
GuillaumeJapan <mauguen.guillaume@gmail.com>
2021-04-07 07:13:10 +0000

    add user signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   
   resources :articles
   
+  get 'signup', to: 'users#new' 
+  post 'users', to: 'users#create' # Create the missing route
+  #resources :users, except: [:new] # Create all the routes except the tailored one for the 'new' action
+   
 end
