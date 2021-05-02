dd032c81c7939d0c691d9dd216431bd22c60fde6
GuillaumeJapan <mauguen.guillaume@gmail.com>
2021-05-02 03:33:26 +0000

    Restricted categories creation to admin only

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email: "john@example.com", password: "john", admin: true)
   end
   
   test "shoul get categories index" do
@@ -12,6 +13,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
   
   test "shoul get new" do
+    sign_in_as(@user, "john")
     get new_category_path
     assert_response :success
   end
@@ -19,6 +21,21 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   test "shoul get show" do
     get category_path(@category)
     assert_response :success
+  end
+  
+  test "should redirect create when admin is not logged in" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category: {name: "books"}}
+    end
+    assert_redirected_to categories_path
+  end
+  
+  test "should create when admin is logged in" do
+    sign_in_as(@user, "john")
+    assert_difference 'Category.count', 1 do
+      post categories_path, params: {category: {name: "books"}}
+    end
+    assert_redirected_to categories_path
   end
   
 end
